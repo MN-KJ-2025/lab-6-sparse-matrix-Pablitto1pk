50e90ea8c0a64ba0f1ba076d02a937f8d53f8455
Pablitto1pk <pawel_krok@hotmail.com>
2025-11-19 12:56:19 +0100

    Main, testy, sprawozdanie poza ostatnim

--- main.py
+++ main.py
@@ -23,8 +23,34 @@ def is_diagonally_dominant(A: np.ndarray | sp.sparse.csc_array) -> bool | None:
             w przeciwnym wypadku `False`.
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
 
+    if A is None:
+        return None
+    if not isinstance(A, (np.ndarray, sp.sparse.csc_array)):
+        return None
+
+    if A.ndim != 2:
+        return None
+
+    m, n = A.shape
+
+    if m != n:
+        return None
+
+    if isinstance(A, np.ndarray):
+        abs_A = np.abs(A)
+        row_sum_abs = np.sum(abs_A, axis=1)
+        diag_abs = np.abs(np.diag(A))
+
+    else:
+        abs_A = A.copy()
+        abs_A.data = np.abs(abs_A.data)
+        row_sum_abs = np.array(abs_A.sum(axis=1)).ravel()
+        diag_abs = np.abs(A.diagonal())
+
+    off_diag_sum = row_sum_abs - diag_abs
+
+    return bool(np.all(diag_abs > off_diag_sum))
 
 def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> float | None:
     """Funkcja obliczająca normę residuum dla równania postaci: 
@@ -40,4 +66,16 @@ def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> float | None:
         (float): Wartość normy residuum dla podanych parametrów.
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
+    if not isinstance(x, np.ndarray) or not isinstance(b, np.ndarray):
+        return None
+
+    if not isinstance(A, (np.ndarray, sp.sparse.spmatrix)):
+        return None
+    m, n = A.shape
+
+    if b.shape != (m,) or x.shape != (n,):
+        return None
+
+    r = b - A @ x
+
+    return float(np.linalg.norm(r))
